317ec2cd2e37fbf618046007ad7e5621a3eed5c6
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-25 23:04:13 +0700

    Fast Inverse Square Root as implemented in Quake III Arena

--- main.cpp
+++ main.cpp
@@ -7,8 +7,30 @@ using namespace std;
 class Solution {
  public:
   int mySqrt(int x) {
-    // FIXME
-    return x;
+    return std::abs(1.0 / Q_rsqrt(x));
+  }
+
+ private:
+  /** Fast Inverse Square Root as implemented in Quake III Arena
+   *
+   * https://archive.softwareheritage.org/browse/content/sha1_git:bb0faf6919fc60636b2696f32ec9b3c2adb247fe/?origin_url=https://github.com/id-Software/Quake-III-Arena&path=code/game/q_math.c&revision=dbe4ddb10315479fc00086f08e25d968b4b43c49&snapshot=4ab9bcef131aaf449a7c01370aff8c91dcecbf5f
+   * @param number
+   * @return
+   */
+  float Q_rsqrt( float number ) {
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y  = number;
+    i  = * ( long * ) &y;						// evil floating point bit level hacking
+    i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+    return y;
   }
 };
 
